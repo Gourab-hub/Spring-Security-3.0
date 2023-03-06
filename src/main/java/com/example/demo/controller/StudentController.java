3424e1c4ee8f3f310bc430699d2a71f3ebeae1be
Gourab Banik <58955646+Gourab-hub@users.noreply.github.com>
2023-03-06 10:54:59 +0530

    EnableMethodSecurity and @PreAuthorize("hasAuthority('ROLE_ADMIN')") implemented.

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,12 +26,14 @@ public class StudentController {
 	}
 
 	@GetMapping("/all")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public List<Student> listAll() {
 		List<Student> listStudents = studentRepository.findAll();
 		return listStudents;
 	}
 	
 	@GetMapping("{id}")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
 	public Optional<Student> getNameById(@PathVariable int id) {
 		Optional<Student> students = studentRepository.findById(id);
 		return students;
