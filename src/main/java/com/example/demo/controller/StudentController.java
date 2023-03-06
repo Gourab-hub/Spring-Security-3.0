4c47c23f27856242712292e97f95cb0c6d9775f7
Gourab Banik <58955646+Gourab-hub@users.noreply.github.com>
2023-03-06 10:21:11 +0530

    Basic Authentication

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello Gb";
+	}
+
+	@GetMapping("/all")
+	public List<Student> listAll() {
+		List<Student> listStudents = studentRepository.findAll();
+		return listStudents;
+	}
+	
+	@GetMapping("{id}")
+	public Optional<Student> getNameById(@PathVariable int id) {
+		Optional<Student> students = studentRepository.findById(id);
+		return students;
+	}
+
+}
